69911244d3c64027cf231a6887919a07660197c2
komalarao <manoharvakamullu1999@gmail.com>
2023-02-20 08:25:03 -0800

    refactor: cabInvoiceGenerator program for caluculate travel cost for multiple rides

--- src/main/java/com/bridgelabz/cabInvoiceGenerator/MoneyGenerator.java
+++ src/main/java/com/bridgelabz/cabInvoiceGenerator/MoneyGenerator.java
@@ -2,13 +2,21 @@ package com.bridgelabz.cabInvoiceGenerator;
 
 public class MoneyGenerator {
 	private static final double MIN_COST_PER_KILOMETER = 10;
-    private static final double COST_FOR_TIME = 1;
-    private static final double MINIMUM_AMOUNT = 5;
+	private static final double COST_FOR_TIME = 1;
+	private static final double MINIMUM_AMOUNT = 5;
 
-    public double travelCost(double distance, double time) {
-        double cost = distance * MIN_COST_PER_KILOMETER + time * COST_FOR_TIME;
-        if (cost < MINIMUM_AMOUNT)
-            return MINIMUM_AMOUNT;
-        return cost;
-    }
+	public double travelCost(double distance, double time) {
+		double cost = distance * MIN_COST_PER_KILOMETER + time * COST_FOR_TIME;
+		if (cost < MINIMUM_AMOUNT)
+			return MINIMUM_AMOUNT;
+		return cost;
+	}
+
+	public double calculateFareForMultipleRides( Ride[] rides) {
+		double cost = 0;
+		for (Ride ride : rides) {
+			cost = cost + this.travelCost(ride.distance, ride.time);
+		}
+		return cost;
+	}
 }
